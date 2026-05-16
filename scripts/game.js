a27ed9092930f7d9d42753bfc7e48262f623a477
jmitchellpcf <mitcjj@farmingdale.edu>
2026-05-16 14:30:21 -0400

    quiz game first run

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const questions = [];
 const quizQuestions = [];
-const score = 0;
+let score = 0;
+let qNumber = 0;
+const form = document.getElementById("quiz");
 
 fs.readFile('../backend/questions.json', 'utf8', (err, jsonString) => {
     if (err) {
@@ -17,18 +19,36 @@ fs.readFile('../backend/questions.json', 'utf8', (err, jsonString) => {
 });
 
 function setQuestions() {
-    for (let i = 0; i < 11; i++) {
+    for (let i = 0; i < 10; i++) {
         let x = Math.floor(Math.random() * questions.length);
         quizQuestions.push(questions[x]);
     }
 }
 
 function getQuestion() {
-
+    let qTitle = document.getElementById("question");
+    qTitle.innerText = "Question #" + (questions[qNumber] + 1);
+    let a = document.getElementById("answer1Label");
+    a.innerText = questions[qNumber]["A"];
+    let b = document.getElementById("answer2Label");
+    a.innerText = questions[qNumber]["B"];
+    let c = document.getElementById("answer3Label");
+    a.innerText = questions[qNumber]["C"];
+    let d = document.getElementById("answer4Label");
+    a.innerText = questions[qNumber]["D"];
 }
 
 function checkAnswer() {
-    let answer = document.getElementById("answer");
+    let answer = form.elements["answer"].value;
+    if (answer === quizQuestions[qNumber].answer) {
+        score++;
+    }
+    qNumber++;
+    if(qNumber >= 10) {
+        endGame();
+    } else {
+        getQuestion();
+    }
 }
 
 function endGame() {
